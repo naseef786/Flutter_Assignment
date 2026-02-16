a07e86de8732dab406a6394978f4a1fe9fc9e5ba
naseef786 <naseefac786@gmail.com>
2026-02-17 03:32:52 +0400

    Implemented Tab Bar Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'screens/add_todo_screen.dart';
+import 'screens/settings_screen.dart';
 
 void main() {
   runApp(TodoApp());
@@ -13,7 +15,39 @@ class TodoApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Todo App',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(),
+      home: MainTabs(),
+    );
+  }
+}
+
+class MainTabs extends StatefulWidget {
+  const MainTabs({super.key});
+
+  @override
+  _MainTabsState createState() => _MainTabsState();
+}
+
+class _MainTabsState extends State<MainTabs> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [HomeScreen(), AddTodoScreen(), SettingsScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(index: _currentIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
